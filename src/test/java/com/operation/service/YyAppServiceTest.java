1388127f18a63140603e2acaac25abc8bb0afdd6
王辉 <wang.hui@rhxtune.com>
2016-03-16 19:04:55 +0800

    测试依赖,以及demo

---
+++ src/test/java/com/operation/service/YyAppServiceTest.java
@@ -0,0 +1,50 @@
+package com.operation.service;
+
+import com.operation.ApplicationMain;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author : Hui.Wang [wang.hui@rhxtune.com]
+ * @version : 1.0
+ * @created on  : 2016-03-16,  下午7:03
+ * @copyright : Copyright(c) 2015  北京瑞华信通信息技术有限公司
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(ApplicationMain.class)
+public class YyAppServiceTest {
+
+    @Test
+    public void testSave() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAll1() throws Exception {
+
+    }
+
+    @Test
+    public void testFindOne() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+}
